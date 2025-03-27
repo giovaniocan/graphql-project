9f715c50e5fd2c0d408ed8c0f9c624b4fc93474f
giovaniocan <giovaniocan@gmail.com>
2025-03-27 13:48:14 -0300

    finalizado estrutura basica do back end

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,23 @@
 import 'reflect-metadata'
 import path from 'node:path';
+import { ApolloServer } from 'apollo-server'
 
 
 import { buildSchema } from 'type-graphql'
+import { UserResolver } from './src/resolvers/UserResolver';
 
 async function main() {
     
     const schema = await buildSchema({
-        resolvers:[],
+        resolvers:[UserResolver],
         emitSchemaFile: path.resolve(__dirname, 'schema.gql')
     })
+
+    const server = new ApolloServer({ schema })
+
+    const { url } = await server.listen(4000)
+
+    console.log("Server running on: ", url)
 
 }
 
