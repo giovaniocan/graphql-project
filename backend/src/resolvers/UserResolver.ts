b7a8db1d063dc30a953781581c9a615306ac5911
giovaniocan <giovaniocan@gmail.com>
2025-03-27 14:09:22 -0300

    finalizado back end

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,11 +1,30 @@
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { User } from '../models/User';
+import crypto from 'crypto'	
 
 @Resolver()
 export class UserResolver {
+    private data: User[] = []
 
-    @Query(() => String)
-    async hello(){
-        return 'hello baby'
+    @Query(() => [User])
+    async users(){
+        return this.data
+    }
+
+    @Mutation(() => User)
+    async createUser(
+        @Arg('name') name: string
+    ){
+        
+
+        const user = {
+            id: crypto.randomUUID(),
+            name
+        }
+
+        this.data.push(user)
+
+        return user
     }
 
 }
